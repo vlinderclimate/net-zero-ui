884e31f3f7a75d3a1f3580f9978abcf085f8064e
ju-vlinder <julia.grebenshchikova@atoma.at>
2021-12-06 12:00:03 +0300

    ci: add semantic versioning, remove release-package action

---
+++ rollup.config.js
@@ -0,0 +1,50 @@
+import commonjs from "@rollup/plugin-commonjs"
+import resolve from "@rollup/plugin-node-resolve"
+import url from "@rollup/plugin-url"
+import copy from "rollup-plugin-copy"
+import generatePackage from "rollup-plugin-generate-package-json"
+import peerDepsExternal from "rollup-plugin-peer-deps-external"
+import typescript from "rollup-plugin-typescript2"
+import remove from "rollup-plugin-delete"
+import multi from "rollup-plugin-multi-input"
+
+const config = {
+  input: ["./src/**/*.ts(x)?"],
+  output: [{ dir: "lib", format: "esm", exports: "named" }],
+  plugins: [
+    multi(),
+    peerDepsExternal(),
+    resolve(),
+    commonjs(),
+    typescript(),
+    generatePackage({
+      baseContents: (pkg) => ({
+        name: pkg.name,
+        author: pkg.author,
+        version: pkg.version,
+        license: pkg.license,
+        keywords: pkg.keywords,
+        devDependencies: pkg.devDependencies,
+        peerDependencies: pkg.peerDependencies
+      })
+    }),
+    copy({
+      targets: [
+        { src: "README.md", dest: "lib/" },
+        { src: "LICENSE.md", dest: "lib/" }
+      ]
+    }),
+    remove({
+      targets: ["lib/*"]
+    }),
+    url({
+      // by default, rollup-plugin-url will not handle font files
+      include: ["**/*.woff", "**/*.woff2", "**/*.eot", "**/*.ttf"],
+      // setting infinite limit will ensure that the files
+      // are always bundled with the code, not copied to /dist
+      limit: Infinity
+    })
+  ]
+}
+
+export default config
