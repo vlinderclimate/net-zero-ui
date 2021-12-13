c9174bf26487da18fccb9438422d395f61ffc9c5
ju-vlinder <julia.grebenshchikova@atoma.at>
2021-12-13 10:39:28 +0300

    feat(Breadcrumbs): update breadcrumbs component

---
+++ src/stories/Breadcrumbs.stories.tsx
@@ -0,0 +1,35 @@
+import { Story, Meta } from "@storybook/react"
+import Link from "@mui/material/Link"
+import Stack from "@mui/material/Stack"
+import Typography from "../components/Typography"
+
+import Breadcrumbs, { BreadcrumbsProps } from "../components/Breadcrumbs"
+
+const links = [
+  <Link href="/">
+    <Typography variant="description" color="primary">
+      Home
+    </Typography>
+  </Link>,
+  <Typography variant="description" color="secondary">
+    Projects
+  </Typography>,
+  <Typography variant="description" color="secondary">
+    Sri Lanka
+  </Typography>
+]
+
+const Template: Story<BreadcrumbsProps> = (args) => {
+  return (
+    <Stack spacing={2}>
+      <Breadcrumbs>{links}</Breadcrumbs>
+    </Stack>
+  )
+}
+
+export const Default = Template.bind({})
+
+export default {
+  title: "Components/Breadcrumbs",
+  component: Breadcrumbs
+} as Meta
