9f6672f13c377768d9dbcda713830c29b50074df
ju-vlinder <julia.grebenshchikova@atoma.at>
2022-07-29 13:41:28 +0200

    fix: remove console warnings

--- src/overrides/MuiTableCell.ts
+++ src/overrides/MuiTableCell.ts
@@ -7,16 +7,13 @@ const MuiTableCell = {
     paddingTop: "11px",
     paddingBottom: "11px",
     paddingRight: "24px",
-    paddingLeft: "0",
+    paddingLeft: "24px",
     borderBottom: "1px solid #D9D9D9",
     backgroundColor: "transparent",
     transition: "border-color 0.2s ease, background-color 0.2s ease",
     fontFeatureSettings: "'pnum' on, 'lnum' on, 'liga' off",
-    "&:not(:first-child)": {
+    "&:not(:first-of-type)": {
       paddingLeft: 0
-    },
-    "&:last-child": {
-      paddingRight: 0
     },
     "@media all and (max-width: 426px)": {
       paddingTop: 7,
